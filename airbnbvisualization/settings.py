7a66a117c3f0fcf1cb3c254506c20fe4b471e871
Chris Fischer <chris.fischer36@gmail.com>
2017-10-27 20:34:39 -0400

    Use pipenv

--- airbnbvisualization/settings.py
+++ airbnbvisualization/settings.py
@@ -16,7 +16,7 @@ import psycopg2
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['https://airbnbvisualization.herokuapp.com/']
+ALLOWED_HOSTS = ['airbnbvisualization.herokuapp.com']
 
 
 # Application definition
@@ -106,13 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
